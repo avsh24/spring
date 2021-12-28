988eabf1cc96298a0aed1d4d0b20f7054e438e00
Admin <avsh24@yandex.ru>
2021-12-28 14:25:22 +0500

    IoC и DI

---
+++ src/IoC_DI/introduction/Test2.java
@@ -0,0 +1,14 @@
+package IoC_DI.introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+        pet.say();
+
+        context.close();
+    }
+}
