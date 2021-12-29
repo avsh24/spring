2b517330740d828b634b6a9371c225b42a640059
Admin <avsh24@yandex.ru>
2021-12-29 14:50:40 +0500

    IoC и DI

--- src/IoC_DI/introduction/Dog.java
+++ src/IoC_DI/introduction/Dog.java
@@ -1,6 +1,10 @@
 package IoC_DI.introduction;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Bow-Wow");
