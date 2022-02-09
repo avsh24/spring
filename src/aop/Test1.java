dc0e851a65015eebf668337b5fe8369b744f5a77
Admin <avsh24@yandex.ru>
2022-02-09 17:04:35 +0500

    AOP

--- src/aop/Test1.java
+++ src/aop/Test1.java
@@ -7,12 +7,14 @@ public class Test1 {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        uniLibrary.getBook();
-        uniLibrary.returnBook();
-        uniLibrary.getMagazine();
+        Book book = context.getBean("book", Book.class);
+        uniLibrary.getBook(book);
 
-        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
-        schoolLibrary.getBook();
+//        uniLibrary.returnBook();
+//        uniLibrary.getMagazine();
+
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+//        schoolLibrary.getBook();
 
         context.close();
     }
