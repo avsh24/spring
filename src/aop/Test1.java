6696b9dcdacc4686691614d588f81b1b0e6931fa
Admin <avsh24@yandex.ru>
2022-02-09 16:27:14 +0500

    AOP

--- src/aop/Test1.java
+++ src/aop/Test1.java
@@ -6,8 +6,13 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
-        Library library = context.getBean("libraryBook", Library.class);
-        library.getBook();
+        UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+        uniLibrary.getBook();
+        uniLibrary.returnBook();
+        uniLibrary.getMagazine();
+
+        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+        schoolLibrary.getBook();
 
         context.close();
     }
