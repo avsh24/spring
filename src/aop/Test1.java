399e945cf4a4c32b6faa229b19ecc34920cd4a65
Admin <avsh24@yandex.ru>
2022-02-10 18:00:09 +0500

    AOP

--- src/aop/Test1.java
+++ src/aop/Test1.java
@@ -7,11 +7,11 @@ public class Test1 {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        Book book = context.getBean("book", Book.class);
-        uniLibrary.getBook(book);
+//        Book book = context.getBean("book", Book.class);
+        uniLibrary.getBook();
+        uniLibrary.getMagazine();
 
-//        uniLibrary.returnBook();
-//        uniLibrary.getMagazine();
+        //        uniLibrary.returnBook();
 
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
 //        schoolLibrary.getBook();
---
+++ src/aop/aspects/LoggingAndSecurityAspect.java
@@ -0,0 +1,21 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAndSecurityAspect {
+    @Before("execution(* get*())")
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvice:" +
+                "попытка получить книгу/журнал");
+    }
+
+        @Before("execution(* get*())")
+    public void beforeGetSecurityAdvice() {
+        System.out.println("beforeGetSecurityAdvice:" +
+                "проверка прав на получение книги/журнала");
+    }
+}
