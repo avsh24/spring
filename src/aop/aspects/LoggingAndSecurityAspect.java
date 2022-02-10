bcd5fbc1ba3eb825d3fbb0024d1dbdce4bb4a413
Admin <avsh24@yandex.ru>
2022-02-10 18:07:55 +0500

    AOP

--- src/aop/aspects/LoggingAndSecurityAspect.java
+++ src/aop/aspects/LoggingAndSecurityAspect.java
@@ -2,18 +2,24 @@ package aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAndSecurityAspect {
-    @Before("execution(* get*())")
+    @Pointcut("execution(* get*())")
+    private void allGetMethods() {
+
+    }
+
+    @Before("allGetMethods()")
     public void beforeGetLoggingAdvice() {
         System.out.println("beforeGetLoggingAdvice:" +
                 "попытка получить книгу/журнал");
     }
 
-        @Before("execution(* get*())")
+        @Before("allGetMethods()")
     public void beforeGetSecurityAdvice() {
         System.out.println("beforeGetSecurityAdvice:" +
                 "проверка прав на получение книги/журнала");
