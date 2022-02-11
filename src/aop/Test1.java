8fa909069b8f5cbc5b28ddd9c7b5de993e771d2a
Admin <avsh24@yandex.ru>
2022-02-11 14:44:28 +0500

    AOP Порядок выполнения Aspect-ов.

--- src/aop/Test1.java
+++ src/aop/Test1.java
@@ -9,11 +9,13 @@ public class Test1 {
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
 //        Book book = context.getBean("book", Book.class);
         uniLibrary.getBook();
-        uniLibrary.returnMagazine();
-        uniLibrary.addBook();
+        uniLibrary.getMagazine();
+
+//        uniLibrary.returnMagazine();
+//        uniLibrary.addBook();
 
 //        uniLibrary.returnBook();
-//        uniLibrary.getMagazine();
+
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
 //        schoolLibrary.getBook();
 
--- src/aop/aspects/LoggingAspect.java
+++ src/aop/aspects/LoggingAspect.java
@@ -3,28 +3,30 @@ package aop.aspects;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public class LoggingAndSecurityAspect {
-    @Pointcut("execution(* aop.UniLibrary.*(..))")
-    private void allMethodsFromUniLibrary() {
-    }
-
-    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
-    private void returnMagazineFromUniLibrary() {
-    }
-
-    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
-    private void allMethodsExceptReturnMagazineFromUniLibrary() {
-    }
-
-    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
-    public void beforeAllMethodsExceptReturnMagazineAdvice() {
-        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: " +
-                "writing Log #10");
-    }
+@Order(1)
+public class LoggingAspect {
+//    @Pointcut("execution(* aop.UniLibrary.*(..))")
+//    private void allMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+//    private void returnMagazineFromUniLibrary() {
+//    }
+//
+//    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+//    private void allMethodsExceptReturnMagazineFromUniLibrary() {
+//    }
+//
+//    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+//    public void beforeAllMethodsExceptReturnMagazineAdvice() {
+//        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: " +
+//                "writing Log #10");
+//    }
 
 //    @Pointcut("execution(* aop.UniLibrary.get*())")
 //    private void allGetMethodsFromUniLibrary() {
@@ -56,19 +58,9 @@ public class LoggingAndSecurityAspect {
 //                "writing Log #3");
 //    }
 
-//    @Pointcut("execution(* get*())")
-//    private void allGetMethods() {
-//    }
-
-//    @Before("allGetMethods()")
-//    public void beforeGetLoggingAdvice() {
-//        System.out.println("beforeGetLoggingAdvice:" +
-//                "попытка получить книгу/журнал");
-//    }
-//
-//        @Before("allGetMethods()")
-//    public void beforeGetSecurityAdvice() {
-//        System.out.println("beforeGetSecurityAdvice:" +
-//                "проверка прав на получение книги/журнала");
-//    }
+    @Before("aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvice: " +
+                "логирование попытки получить книгу/журнал");
+    }
 }
