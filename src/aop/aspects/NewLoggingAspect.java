dd4b01e2514401796e4a85abeb98ae880d89a754
Admin <avsh24@yandex.ru>
2022-02-13 21:07:25 +0500

    AOP

--- src/aop/aspects/NewLoggingAspect.java
+++ src/aop/aspects/NewLoggingAspect.java
@@ -13,15 +13,17 @@ public class NewLoggingAspect {
             ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice: " +
                 "в библиотеку пытаются вернуть книгу");
-
-        long begin = System.currentTimeMillis();
-        Object targetMethodResult = proceedingJoinPoint.proceed();
-        long end = System.currentTimeMillis();
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: " +
+                    "было поймано исключение " + e);
+            throw e;
+        }
 
         System.out.println("aroundReturnBookLoggingAdvice: " +
                 "в библиотеку успешно вернули книгу");
-        System.out.println("aroundReturnBookLoggingAdvice: " +
-                "выполнил работу за " + (end - begin) + " миллисекунд");
         return targetMethodResult;
     }
 }
