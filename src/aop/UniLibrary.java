d3c99da6139d4af46917df429f5a7a4a5132ce7a
Admin <avsh24@yandex.ru>
2022-02-13 19:09:11 +0500

    AOP

--- src/aop/UniLibrary.java
+++ src/aop/UniLibrary.java
@@ -9,9 +9,9 @@ public class UniLibrary extends AbstractLibrary {
         System.out.println("---------------------------------------------------");
     }
 
-    public void returnBook() {
+    public String returnBook() {
         System.out.println("Мы возвращаем книгу в UniLibrary");
-        System.out.println("---------------------------------------------------");
+        return "Spring 5 для профессионалов.";
     }
 
     public void getMagazine() {
